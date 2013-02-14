319489249d2bbc31deb6fa0e09a5f6bfad8752d4
Wilmer Henao <Wilmer.Henao@nyu.edu>
2013-02-14 17:08:53 -0500

    a few more updates

--- csrc/linesearch.h
+++
@@ -1,3 +0,0 @@
-double linesearch_ww 
-(double x[], double *f, double g[], double d[], double C1, double C2,
-int n, void(*testFunction)(double*, double*, double*, int), int*nfeval, double ftarget,int*exitflag);
