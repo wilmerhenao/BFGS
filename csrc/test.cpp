f12c5da0f27e505e6cde1cadba8692770f923d89
Wilmer Henao <Wilmer.Henao@nyu.edu>
2013-02-15 15:51:16 -0500

    commit not working.  Just uplading to hithub to work on my laptop

--- csrc/test.cpp
+++ csrc/test.cpp
@@ -17,7 +17,8 @@
 #include <qd/dd_real.h>
 
 template<typename T>
-struct algoparameters{
+class algoparameters{
+public:
     T ftarget, gnormtol, taux, taud;
     int J, maxit, echo, n, lm, m;
     const char * datafilename;
@@ -25,12 +26,14 @@ struct algoparameters{
     double * info;
     T * xf;
     double * x0;    
-    algoparameters(int);
+    void(*test_fun_ptr)(T*, T*, T*, int);
+    algoparameters(int, char *);
     void generateXF();
+  void BFGSfunction();
 };
 
 template<typename T>
-algoparameters<T>::algoparameters(int k){
+algoparameters<T>::algoparameters(int k, char * locfunc){
     n = k;
     lm=0;
     m = 7;
@@ -46,6 +49,9 @@ algoparameters<T>::algoparameters(int k){
     info = new double[4];
     xf = new T[n];
     x0 = new double[n];
+    void *prg_handle = dlopen("../lib/libtestfun.so", RTLD_NOW); 
+    test_fun_ptr = (void(*FPTR)(T*, T*, T*, int)) dlsym(prg_handle, locfunc);
+    generateXF();
 }
 
 template <typename T> 
@@ -60,119 +66,32 @@ void algoparameters<T>::generateXF(){
 	xf[j] = (T)x0[j];
 }
 
+template<typename T>
+void algoparameters<T>::BFGSfunction(){
+    bfgs<T>(xf, fopt, n, lm, m, ftarget, gnormtol, maxit, J, taux, taud, 
+		 echo, test_fun_ptr, datafilename, info);
+}
+
 int main(int argc, char *argv[]){    
     unsigned int old_cw;
     fpu_fix_start(&old_cw);//necessary for double-double (see Sherry's paper 
     cout.precision(32);	
     
-    typedef void(*test_fun_ptr_type)(double*, double*, double*, int);
+    /*typedef void(*test_fun_ptr_type)(double*, double*, double*, int);
     test_fun_ptr_type testFunction;
     typedef void(*test_fun_ptr_type_dd)(dd_real*, dd_real*, dd_real*, int);
-    test_fun_ptr_type_dd testFunction_dd;
+    test_fun_ptr_type_dd testFunction_dd;*/
 	
-    algoparameters<double> doubleparameters(atoi(argv[3]));
-    algoparameters<dd_real> dd_realparameters(atoi(argv[3]));
-    /*
-    int n;
-    int lm;
-    int m;
+    algoparameters<double> doubleparameters(atoi(argv[3]), argv[1]);
+    algoparameters<dd_real> dd_realparameters(atoi(argv[3]), argv[1]);
+
+    //void *prg_handle = dlopen("../lib/libtestfun.so", RTLD_NOW);    
     
-    double ftarget;
-    double gnormtol;
-    double taux, taud;
-    dd_real ftarget_dd;
-    dd_real gnormtol_dd;
-    dd_real taux_dd, taud_dd;
-    int J;
-    int maxit;
-    int echo;
-    const char * datafilename;
-    const char * datafilename_dd;
-    */
-    // Perhaps this RTLD_NOW here is wasting some time, 
-    //this is something that could be
-    // investigated further
-    // Catch error if dlopen returns NULL (see wikipedia)
-    // It would be nice to close this library
-    void *prg_handle = dlopen("../lib/libtestfun.so", RTLD_NOW);    
-    
-    /* Declare needed variables (output) */
-    /*
-    double * fopt = new double;
-    dd_real * fopt_dd = new dd_real;
-    double * info = new double[4];
-    double * info_dd = new double[4];
-    */
-    /* Assign pointers: */
-    /* input: */
-    testFunction = (test_fun_ptr_type) dlsym(prg_handle, argv[1]);
-    testFunction_dd = (test_fun_ptr_type_dd) dlsym(prg_handle, argv[2]);
-    /*
-    n            = atoi(argv[3]);	//# variables
-    lm           = 0;			//lbfgs or not
-    m            = 7;			//lbfgs paramete
-    J            = fmin(15,ceil(2*n/3));
-    taux         = 1e-16;		//stpsize tolerance
-    taud         = 1e-4;
-    ftarget      = -1e100;		//fvalquit
-    gnormtol     = 0;
-    maxit        = 10e8;		//maximum iterations
-    echo         = 2;			//print level
-    datafilename = "stddump.txt";
-    datafilename_dd = "stddump_dd.txt";
+    //testFunction = (test_fun_ptr_type) dlsym(prg_handle, argv[1]);
+    //testFunction_dd = (test_fun_ptr_type_dd) dlsym(prg_handle, argv[2]);
 
-    taux_dd         = 1e-16;		//stepsize tolerance
-    taud_dd         = 1e-4;
-    ftarget_dd      = -1e100;		//fvalquit
-    gnormtol_dd     = 0.0;
-    */
-    /*
-    double *x0 = new double[n];        
-    double *xf = new double[n];
-    dd_real *xf_dd = new dd_real[n];
-
-    struct timeval time;
-    long useconds;
-    gettimeofday(&time, NULL);
-    useconds = time.tv_usec;	//pseudo-random number generator seed
-
-    srand(useconds);
-    rand_real_vec<double>(x0, n, -1, 1);*/
-    /*
-      x0[0] = -1;
-      for (int i=1; i<n; i++) {
-      x0[i] = 1;
-      }
-    */
-
-    /* output: */    
-    /* Create matrices for the return arguments. */
-
-    /* Copy xf <-- x0 so x0 is still available after BFGS run */ 	
-    /*for (int j=0; j<n; j++) {
-	xf[j] = xf_dd[j] = x0[j];
-    }*/
-    // Call the BFGS D and DD subroutine.
-
-    doubleparameters.generateXF();
-    dd_realparameters.generateXF();
-
-    bfgs<double>(doubleparameters.xf, doubleparameters.fopt, 
-		 doubleparameters.n, doubleparameters.lm, doubleparameters.m,
-		 doubleparameters.ftarget, doubleparameters.gnormtol, 
-		 doubleparameters.maxit, doubleparameters.J, 
-		 doubleparameters.taux, doubleparameters.taud, 
-		 doubleparameters.echo, testFunction, 
-		 doubleparameters.datafilename, doubleparameters.info);
-
-    bfgs<dd_real>(dd_realparameters.xf, dd_realparameters.fopt, 
-		  dd_realparameters.n, dd_realparameters.lm, 
-		  dd_realparameters.m, dd_realparameters.ftarget, 
-		  dd_realparameters.gnormtol, dd_realparameters.maxit, 
-		  dd_realparameters.J, dd_realparameters.taux, 
-		  dd_realparameters.taud, dd_realparameters.echo, 
-		  testFunction_dd, dd_realparameters.datafilename, 
-		  dd_realparameters.info);
+    doubleparameters.BFGSfunction();
+    dd_realparameters.BFGSfunction();
     
     fpu_fix_end(&old_cw);
 }
