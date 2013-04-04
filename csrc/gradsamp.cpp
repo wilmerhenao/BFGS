c30ff52e90a2dbbd67eca43e782f42f2e916c032
Wilmer Henao <wilmer.henao@caa.columbia.edu>
2013-04-04 18:19:40 -0400

    GPL licenses added

--- csrc/gradsamp.cpp
+++ csrc/gradsamp.cpp
@@ -1,4 +1,20 @@
-
+/*
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  BFGS tools Copyright (C) 2013  Wilmer Henao
+%%  This program is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
+%%
+%%  This program is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%
+%%  You should have received a copy of the GNU General Public License
+%%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+*/
 
 bool themin(double * x, double * gradpoints, size_t i, const size_t n){
   // This function calculates the minimum coordinate distance from x to gradpoints
